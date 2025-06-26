bc05e7e6233d00df5e9d3be30d14ba4562351424
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2025-06-26 11:17:02 +0500

    feat: update iframe allow attributes in ServiceDetail component for improved functionality

--- components/ServiceDetail.jsx
+++ components/ServiceDetail.jsx
@@ -104,7 +104,7 @@ const ServiceDetail = ({ service, onClose }) => {
                 <iframe
                   src={item}
                   title={`Portfolio video ${index + 1}`}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen
                   className="absolute top-0 left-0 w-full h-full rounded"
