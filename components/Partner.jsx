1e88734feaf0d47d21523a73e804ba0b659b58d6
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2025-06-28 20:47:44 +0500

    Hopefully Last commit for this project

--- components/Partner.jsx
+++ components/Partner.jsx
@@ -8,13 +8,13 @@ export default function PartnerClients() {
   const [isMobile, setIsMobile] = useState(false);
 
   const partners = [
-    { id: 1, name: "Quranic Media", image: "/1.png" },
-    { id: 2, name: "Client 2", image: "/2.png" },
-    { id: 3, name: "Client 3", image: "/3.png" },
-    { id: 4, name: "Client 4", image: "/4.png" },
-    { id: 5, name: "Client 5", image: "/5.png" },
-    { id: 6, name: "Client 6", image: "/6.png" },
-    { id: 7, name: "Quranic Media", image: "/7.png" },
+    { id: 1, name: "Quranic Media", image: "/Artboard 8.png" },
+    { id: 2, name: "Client 2", image: "/Artboard 2.png" },
+    { id: 3, name: "Client 3", image: "/Artboard 11.png" },
+    { id: 4, name: "Client 4", image: "/Artboard 4.png" },
+    { id: 5, name: "Client 5", image: "/Artboard 10.png" },
+    { id: 6, name: "Client 6", image: "/Artboard 6.png" },
+    { id: 7, name: "Quranic Media", image: "/Artboard 7.png" },
   ];
 
   const allPartners = [...partners, ...partners];
@@ -58,7 +58,7 @@ export default function PartnerClients() {
               {partners.map((i) => (
                 <div
                   key={i.id}
-                  className="w-[100px] h-[100px] rounded-full border-2 border-secondary overflow-hidden relative transition-all duration-700 ease-out hover:scale-105"
+                  className="w-[100px] h-[100px] rounded-full border-2 border-secondary overflow-hidden relative transition-all duration-700 ease-out "
                 >
                   <Image
                     src={i.image}
